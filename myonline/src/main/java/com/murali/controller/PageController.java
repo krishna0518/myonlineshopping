0b401021479a31b42beaee07afa454d893cbbf99
krishna0518 <heerekarmurali@gmail.com>
2019-12-28 13:20:50 +0530

    static pages

---
+++ myonline/src/main/java/com/murali/controller/PageController.java
@@ -0,0 +1,20 @@
+package com.murali.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value= {"/","/index","/home"})
+	public ModelAndView index()
+	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting","welcome to spring world");
+		
+		return mv;
+		
+	}
+
+}
